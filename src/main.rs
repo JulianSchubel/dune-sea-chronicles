6f0f2f9fb055b1a331666ec07249057c916d5ab0
Julian Schubel <schubeljulian@gmail.com>
2024-11-11 22:07:44 +0200

    Remove superfluous while loop in main

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use dune_sea_chronicles::prelude::*;
 
 fn main() -> Result<(), String> {
     let mut game_state: State = State::init();
-    while game_state.run {
-        game_state.start();
-    }
+    game_state.start();
     Ok(())
 }
